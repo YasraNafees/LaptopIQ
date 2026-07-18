f217b0743ce84986123663f492f4a1b9017b5af1
YasraNafees <yasranafees419@gmail.com>
2026-07-18 07:37:20 +0500

    Add  api link update fronted

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://laptop-iq.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
